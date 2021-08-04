00bea0cb1d159b24e13c20020a747b381d6ed8ab
Your Name <you@example.com>
2021-08-04 19:33:16 +0800

    feat(deps): 依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import legacy from '@vitejs/plugin-legacy' //兼容低版本浏览器插件
 import styleImport from 'vite-plugin-style-import'
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
 import { resolve } from 'path'
